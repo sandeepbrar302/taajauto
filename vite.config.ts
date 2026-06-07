d59be49b0d1adf44de43f4634b6066c112f21217
sandeepbrar302 <sandeeobrar302@gmail.com>
2026-06-07 00:06:03 -0400

    app build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 
 function figmaAssetResolver() {
@@ -23,6 +24,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    viteSingleFile(),
   ],
   resolve: {
     alias: {
