a12d950afbf10ff88fa596ed43d63ddd26a749ba
sandeepbrar302 <sandeeobrar302@gmail.com>
2026-06-07 00:17:51 -0400

    Configure Vite to build to docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -35,4 +35,8 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
